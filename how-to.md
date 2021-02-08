a9fdf50f98ce2c2de54806e0730ddbc244359fc6
Matheus-Ernesto <matheusernestodossantos231@gmail.com>
2021-02-08 10:59:55 -0300

    Check apt_dir

---
+++ how-to.md
@@ -0,0 +1,9 @@
+How to use.
+
+Open your terminal.
+
+Go to folder with the script
+
+execute with ./ (matbe need chmod +x updater.sh)
+
+example: sudo ./updater.sh
--- updater.sh
+++ updater.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 #AUTHOR:Matheus-Ernesto-dos-Santos
-#UPDATE: 01-02-2021 - 16:20 by (AUTHOR)
+#UPDATE: 08-02-2021 - 10:48 by (AUTHOR)
 
-echo "----Update------"
-sudo apt update
-echo "----Upgrade-----"
-sudo apt upgrade
-echo "----Autoremove--"
-sudo apt autoremove
+APT_DIR="/etc/apt"
+if [ -d $APT_DIR ]; then
+    echo "----Update------"
+    sudo apt update
+    echo "----Upgrade-----"
+    sudo apt upgrade
+    echo "----Autoremove--"
+    sudo apt autoremove
+fi
 echo "----End---------"
 read -s -p "Press [ENTER] key to exit."
 echo ""
