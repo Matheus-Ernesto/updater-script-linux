6f7856595094e2b26763bc394bc5fc431815f245
Matheus-Ernesto <matheusernestodossantos231@gmail.com>
2021-02-09 08:14:15 -0300

    UPDATED LICENSE TO GPL

--- how-to.md
+++ how-to.md
@@ -4,6 +4,6 @@ Open your terminal.
 
 Go to folder with the script
 
-execute with ./ (matbe need chmod +x updater.sh)
+execute with ./ (maybe need chmod +x updater.sh)
 
 example: sudo ./updater.sh
--- updater.sh
+++ updater.sh
@@ -3,6 +3,12 @@
 #AUTHOR:Matheus-Ernesto-dos-Santos
 #UPDATE: 08-02-2021 - 10:48 by (AUTHOR)
 
+echo "updater Copyright (C) 2021  Matheus-Ernesto"
+echo "This program comes with ABSOLUTELY NO WARRANTY;"
+echo "This is free software, and you are welcome to redistribute it"
+echo "under certain conditions;"
+echo "for more info, access <https://www.gnu.org/licenses/>"
+
 APT_DIR="/etc/apt"
 if [ -d $APT_DIR ]; then
     echo "----Update------"
